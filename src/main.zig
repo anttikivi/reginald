7abcd41f6a4c2c6660f9d0470ad368bdac93d0af
Antti Kivi <antti@anttikivi.fi>
2025-07-29 23:02:09 +0300

    feat: add check for no arguments

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const builtin = @import("builtin");
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 
 const native_os = builtin.target.os.tag;
@@ -19,4 +20,22 @@ pub fn main() !void {
     var arena_instance = std.heap.ArenaAllocator.init(gpa);
     defer arena_instance.deinit();
     const arena = arena_instance.allocator();
+
+    const args = try std.process.argsAlloc(arena);
+    assert(args.len > 0);
+
+    return mainArgs(gpa, arena, args);
+}
+
+fn mainArgs(gpa: Allocator, arena: Allocator, args: []const []const u8) !void {
+    if (args.len <= 1) {
+        var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+        const w = bw.writer();
+        try w.writeAll("usage!\n");
+        try bw.flush();
+        return;
+    }
+
+    _ = gpa;
+    _ = arena;
 }
