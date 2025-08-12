3e35610f3c1327d0dd5d5aa662c2114bf46bc0a2
Antti Kivi <antti@anttikivi.fi>
2025-08-12 08:08:31 +0300

    refactor: break the TOML parser into multiple files

--- src/toml.zig
+++ src/toml.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 
-pub const Value = @import("toml/decoder.zig").Value;
-pub const Array = @import("toml/decoder.zig").Array;
-pub const Table = @import("toml/decoder.zig").Table;
-pub const Datetime = @import("toml/decoder.zig").Datetime;
-pub const Date = @import("toml/decoder.zig").Date;
-pub const Time = @import("toml/decoder.zig").Time;
+pub const Value = @import("toml/value.zig").Value;
+pub const Array = @import("toml/value.zig").Array;
+pub const Table = @import("toml/value.zig").Table;
+pub const Datetime = @import("toml/value.zig").Datetime;
+pub const Date = @import("toml/value.zig").Date;
+pub const Time = @import("toml/value.zig").Time;
 
 pub const Diagnostics = @import("toml/decoder.zig").Diagnostics;
-
 pub const parse = @import("toml/decoder.zig").parse;
 pub const parseWithDiagnostics = @import("toml/decoder.zig").parseWithDiagnostics;
 
