aa6f8bb6bd2683732cdf077835bae3650cc21327
Antti Kivi <antti@anttikivi.com>
2026-03-04 18:35:33 +0200

    chore: license the project under `GPL-3.0-or-later`

--- build.zig
+++ build.zig
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: © 2026 Antti Kivi <antti@anttikivi.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
